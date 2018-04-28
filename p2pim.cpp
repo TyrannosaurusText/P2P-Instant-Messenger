7e0c54db1c798d2c1f776881ae3d499d222d9580
Wenzhe Huang <huhuang@ucdavis.edu>
2018-04-28 13:24:31 -0700

    Very meaningful commit message

--- p2pim.cpp
+++ p2pim.cpp
@@ -3,6 +3,7 @@
 #include <functional>
 #include <cstring>
 #include <unordered_map>
+#include <arpa/inet.h>
 
 #define DEBUG 1
 #ifdef DEBUG
@@ -10,8 +11,14 @@
 #else
 #define dprint(string, ...) 
 #endif
+
+void ERROR_HANDLING();
+
+
+
 std::unordered_map<std::string, int> commandswitch;
-//std::string userName = std::string(getenv("USERNAME"));
+std::string userName = getenv("USER");
+char hostName[1024];
 int udpPort = 50550;
 int tcpPort = 50551;
 int initTimeout = 5;
@@ -20,67 +27,101 @@ int maxTimeout = 60;
 std::string optErr;
 
 
+
+
 int main(int argc, char** argv) {
 	
-	dprint("hi: \n", 0);
-	commandswitch["-u"] = 1;
-	commandswitch["-up"] = 2;
-	commandswitch["-tp"] = 3;
-	commandswitch["-dt"] = 4;
-	commandswitch["-dt"] = 5;
-	commandswitch["-pp"] = 6;
-/*     for(int i = 1; i < argc; i++) {
-        optErr = argv[i];
-        if(strcmp("-u", argv[i]) == 0) {
-            if(i == argc - 1 || argv[i + 1][0] == '-')
-                goto ERROR_HANDLING;
+    gethostname(hostName, 1024);
+    commandswitch["-u"] = 1;
+    commandswitch["-up"] = 2;
+    commandswitch["-tp"] = 3;
+    commandswitch["-dt"] = 4;
+    commandswitch["-dt"] = 5;
+    commandswitch["-pp"] = 6;
 
-            userName = argv[i];
-        }
-        else if(strcmp("-up", argv[i]) == 0) {
-            if(i == argc - 1 || argv[i + 1][0] == '-')
-                goto ERROR_HANDLING;
-        }
-        else if(strcmp("-tp", argv[i]) == 0) {
-            if(i == argc - 1 || argv[i + 1][0] == '-')
-                goto ERROR_HANDLING;
-        }
-        else if(strcmp("-dt", argv[i]) == 0) {
-            if(i == argc - 1 || argv[i + 1][0] == '-')
-                goto ERROR_HANDLING;
-        }
-        else if(strcmp("-dm", argv[i]) == 0) {
-            if(i == argc - 1 || argv[i + 1][0] == '-')
-                goto ERROR_HANDLING;
-        }
-        // else if(strcmp("-pp", argv[i]) == 0) {
-            
-        // }
-    } */
 	for(int i = 1; i < argc; i++){
 		dprint("val: %s\n", argv[i]);
+        optErr = argv[i];
+
+        if(commandswitch.find(argv[i]) != commandswitch.end()) {
+            if(i == argc - 1 || argv[i + 1][0] == '-')
+                    ERROR_HANDLING();
+        }
+
 		switch(commandswitch[argv[i]])
 		{
-			case 1: 
-			case 2: 
-			case 3: 
-			case 4: 
-			case 5: 
-			case 6: 
-				commandswitch[argv[i]]==-1;
-				if(i == argc - 1 || argv[i + 1][0] == '-')
-					goto ERROR_HANDLING;
-				break;
-			default: 
+			case 1: {
+                commandswitch[argv[i]] = -1;
+                userName = argv[i + 1];
+                break;
+            }
+			case 2: {
+                commandswitch[argv[i]] = -1;
+                udpPort = atoi(argv[i + 1]);
+                break;
+            }
+			case 3: {
+                commandswitch[argv[i]] = -1;
+                tcpPort = atoi(argv[i + 1]);
+                break;
+            }
+			case 4: {
+                commandswitch[argv[i]] = -1;
+                initTimeout = atoi(argv[i + 1]);
+                break;
+            }
+			case 5: {
+                commandswitch[argv[i]] = -1;
+                maxTimeout = atoi(argv[i + 1]);
+                break;
+            }
+            // TODO:
+			// case 6: {
+   //              commandswitch[argv[i]] = -1;
+   //              initTimeout = atoi(argv[i + 1]);
+   //              break;
+   //          }
+			default: {
 				dprint("default\n",0);
-			break;
+                ERROR_HANDLING();
+            }
 		}
 	}
+
+    dprint("Username = %s\n", userName.c_str());
+    // TODO: Hostname = sp4.cs.ucdavis.edu
+    dprint("Hostname = %s\n", hostName);
+    dprint("UDP Port = %d\n", udpPort);
+    dprint("TCP Port = %d\n", tcpPort);
+    dprint("Mintimeout = %d\n", initTimeout);
+    dprint("Maxtimeout = %d\n", maxTimeout);
+
+
+    // Construct discovery message
+    int discoveryMsgLen = 4 + 2 + 2 + 2 + strlen(hostName) + 1 + userName.length() + 1;
+    uint8_t discoveryMsg[discoveryMsgLen];
+    bzero(discoveryMsg,discoveryMsgLen);
+
+    memcpy((uint32_t*)discoveryMsg, "P2PI", 4);
+    *((uint16_t*)discoveryMsg + 2) = htons(1);
+    *((uint16_t*)discoveryMsg + 3) = htons(udpPort);
+    *((uint16_t*)discoveryMsg + 4) = htons(tcpPort);
+    memcpy((uint16_t*)discoveryMsg + 5, hostName, strlen(hostName));
+    memcpy((uint16_t*)discoveryMsg + 5 + strlen(hostName) + 1, userName.c_str(), userName.length());
+
+    for(int i = 0; i < discoveryMsgLen; i++)
+    {
+        dprint("%c", discoveryMsg[i]);
+    }
+    dprint("\n", 0);
+    // Send discovery message
+
 	
     return 0;
+}
 
 
-ERROR_HANDLING:
+void ERROR_HANDLING(){
     fprintf(stderr, "./p2pim: option requires an argument -- '%s'\n", optErr.c_str());
 	exit(1);
 }
