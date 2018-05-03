8e24496e45e27b3eed44ad4acde60175322d5fcd
Wenzhe Huang <huhuang@ucdavis.edu>
2018-05-03 15:06:21 -0700

    Reply to broadcast not working yet, need DNS?

--- p2pim.cpp
+++ p2pim.cpp
@@ -1,7 +1,7 @@
 #include "p2pim.h"
 
 
-#define MAX_REPLY_MSG_LEN (10 + 64 + 32 + 2)
+#define MAX_UDP_MSG_LEN (10 + 64 + 32 + 2)
 
 
 enum Option {USERNAME, UDP_PORT, TCP_PORT, MIN_TIMEOUT, MAX_TIMEOUT, HOST};
@@ -10,6 +10,15 @@ static std::unordered_map<std::string, int> optionMap {
     {"-u", USERNAME}, {"-up", UDP_PORT}, {"-tp", TCP_PORT}, 
     {"-dt", MIN_TIMEOUT}, {"dm", MAX_TIMEOUT}, {"pp", HOST}
 };
+
+
+struct Host {
+    std::string userName;
+    std::string hostName;
+    int udpPort;
+    int tcpPort;
+};
+
 
 std::string userName = getenv("USER");
 std::string hostName;
@@ -20,15 +29,303 @@ int maxTimeout = 60000;
 // int destPort = ls
 std::string optErr;
 
+uint8_t udpMsg[MAX_UDP_MSG_LEN];
+int udpMsgLen;
+std::unordered_map<std::string, struct Host> hostMap;
 
-int udpSockFd, enable = 1;
-struct sockaddr_in serverAddr, clientAddr;
+int udpSockFd, tcpSockFd, enable = 1;
+struct sockaddr_in udpServerAddr, clientAddr;
 socklen_t clientAddrLen; 
-struct pollfd pollFd;
+struct pollfd pollFd[2];
 
 
 
 int main(int argc, char** argv) {
+    // Setup signal handler
+    if(signal(SIGINT, SIGINT_handler) == SIG_ERR)
+        die("Failed to catch signal");
+
+    parseOptions(argc, argv);
+
+    // dprint("Username = %s\n", userName.c_str());
+    // // TODO: Hostname = sp4.cs.ucdavis.edu
+    // dprint("Hostname = %s\n", hostName.c_str());
+    // dprint("UDP Port = %d\n", udpPort);
+    // dprint("TCP Port = %d\n", tcpPort);
+    // dprint("Mintimeout = %d\n", minTimeout);
+    // dprint("Maxtimeout = %d\n", maxTimeout);
+
+    initUDPMsg();
+    setupSocket();
+
+    pollFd[0].fd = udpSockFd;
+    pollFd[0].events = POLLIN;
+    // pollFd[1].fd = tcpSockFd;
+    // pollFd[1].events = POLLIN;
+    int baseTimeout = minTimeout;
+    timeval start,end;
+    int timePassed;
+    int currTimeout = 0;
+
+
+    // When no host is available and maxTimeout not exceeded, discovery hosts
+    while(baseTimeout <= maxTimeout * 1000) {
+        
+        if(currTimeout <= 0 && hostMap.empty()) {
+            // if(sendto(udpSockFd, udpMsg, udpMsgLen, 0, 
+            //     (struct sockaddr*)&udpServerAddr, sizeof(udpServerAddr)) < 0) {
+            //     die("Failed to send discovery message");
+
+            // }
+            sendUDPMessage(DISCOVERY);
+
+            currTimeout = baseTimeout;
+            dprint("Sending discovery\n", 0);
+        }
+
+        // Wait for reply message
+        gettimeofday(&start, NULL);
+        int rc = poll(pollFd, 1, currTimeout);
+        gettimeofday(&end, NULL);
+        timePassed = ((end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec) / 1000;
+        currTimeout -= timePassed;
+
+        // dprint("RC is %d\n", rc);
+
+        // Timeout event
+        if(0 == rc) {
+            if(hostMap.empty()) {
+                dprint("TIMEOUT\n", 0);
+
+                // Double current timeout
+                baseTimeout *= 2;
+                dprint("%d\n", currTimeout);
+            }
+
+        }
+        else if(rc > 0) {
+            std::string newHostName, newUserName;
+
+            dprint("RECV\n", 0);
+
+            int recvLen;
+
+            // Reply message
+            uint8_t incomingMsg[MAX_UDP_MSG_LEN];
+            clientAddrLen = sizeof(clientAddr);
+
+            // UDP packet
+            if(pollFd[0].revents == POLLIN) {
+                recvLen = recvfrom(udpSockFd, incomingMsg, MAX_UDP_MSG_LEN, 0,
+                    (struct sockaddr*)&clientAddr, &clientAddrLen);
+
+                if(recvLen > 0) {
+                    int type = getType(incomingMsg);
+
+                    dprint("Received type: %d\n", type);
+                    for(int i = 0; i < recvLen; i++)
+                    {
+                        dprint("%c", incomingMsg[i]);
+                    }
+
+
+                    switch (type) {
+                        case DISCOVERY: {
+                            // Add host to hostMap if not self discovery and not in
+                            // map already
+                            if(memcmp(incomingMsg + 6, udpMsg + 6, udpMsgLen - 6)) {
+                                dprint("Hi friend\n", 0);
+                                addNewHost(incomingMsg);
+                                sendUDPMessage(REPLY);
+                                std::string userName_a, hostName_a;
+                                getHostNUserName(udpMsg, hostName_a, userName_a);
+                                dprint("Replying as %s\n", userName_a.c_str());
+                            }
+                            else
+                                dprint("Self discovery\n", 0);
+
+                            // Send reply message
+
+                            // Wait for tcp connection
+
+                            break;
+                        }
+
+                        case REPLY: {
+                            // Add host to map if not in map already
+                            if(!findHost(incomingMsg)) {
+                                std::string userName_a, hostName_a;
+                                getHostNUserName(incomingMsg, hostName_a, userName_a);
+                                dprint("New host %s\n", userName_a.c_str());
+                                addNewHost(incomingMsg);
+                            }
+
+                            // Try to initiate tcp connection with host
+
+                            break;
+                        }
+
+                        case CLOSING: {
+                            // Remove host from map
+
+                            // If no more host is available, go back to discovery
+                            if(hostMap.empty()) {
+                                currTimeout = 0;
+
+                                // Resets timeout value
+                                baseTimeout = minTimeout;
+                            }
+                            break;
+                        }
+                    }
+                    // getHostNUserName(incomingMsg, newHostName, newUserName);
+                    // dprint("%s %s\n", newHostName.c_str(), newUserName.c_str());
+                    // dprint("%d %d\n", clientAddr.sin_addr.s_addr, clientAddr.sin_port);
+                }
+            }
+
+            // TCP packet
+            if(pollFd[1].revents == POLLIN) {
+
+            }
+        }
+        else
+            dprint("ERROR\n", 0);
+    }
+
+    return 0;
+}
+
+
+
+void optionError(char** argv){
+    fprintf(stderr, "%s: option requires an argument -- '%s'\n", argv[0], optErr.c_str());
+    exit(1);
+}
+
+
+
+std::string getHostName()
+{
+    char buffer[256];
+    
+    if(-1 == gethostname(buffer, 255)){
+        fprintf(stderr, "Unable to resolve host name.");
+        exit(-1);
+    }
+
+    struct hostent* localHostEntry = gethostbyname(buffer);
+    strcpy(buffer, localHostEntry->h_name);
+    return std::string(buffer);
+}
+
+
+
+// Internal syscall errors
+void die(const char *message) {
+    perror(message);
+    exit(1);
+}
+
+
+
+int getType(uint8_t* message) {
+    return ntohs(*((uint16_t*)message + 2));
+}
+
+
+
+void getHostNUserName(uint8_t* message, std::string& hostName, std::string& userName) {
+    hostName = (char*)(message + 10);
+    userName = (char*)(message + 10 + hostName.length() + 1);
+}
+
+
+
+void getPorts(uint8_t* message, int& udpPort, int& tcpPort) {
+    udpPort = ntohs(*((uint16_t*)message + 3));
+    tcpPort = ntohs(*((uint16_t*)message + 4));
+}
+
+
+
+void dump(std::string msg)
+{
+    for(int i = 0; i < msg.length(); i++)
+    {
+        dprint("%d %c \n", msg[i], msg[i]);
+    }
+    dprint("\n", 0);
+}
+
+
+
+void checkIsNum(char* str) {
+    for(int i = 0; i < strlen(str); i++) {
+        if(!isdigit(str[i])) {
+            fprintf(stderr, "Input %s is not a number!\n", str);
+            exit(1);
+        }
+    }
+}
+
+
+
+void checkPortRange(int portNum) {
+    if(1 > portNum || 65535 < portNum) {
+        fprintf(stderr, "Invalid port \"%d\", must be in range [1 65,535]\n", portNum);
+        exit(1);
+    }
+}
+
+
+
+void sendUDPMessage(int type) {
+    *((uint16_t*)udpMsg + 2) = htons(type);  
+
+    std::string userName_a, hostName_a;
+    getHostNUserName(udpMsg, hostName_a, userName_a);
+    dprint("Sendin udp message type %d as %s\n", type, userName_a.c_str());
+    dprint("destination: %d", clientAddr.sin_addr.s_addr);
+    if(type == REPLY) {
+        //clientAddr.sin_port = htons(udpPort);
+        if(sendto(udpSockFd, udpMsg, udpMsgLen, 0, 
+            (struct sockaddr*)&clientAddr, sizeof(clientAddr)) < 0) {
+            die("Failed to send unicast message");
+        }
+    }
+    else {
+        if(sendto(udpSockFd, udpMsg, udpMsgLen, 0, 
+            (struct sockaddr*)&udpServerAddr, sizeof(udpServerAddr)) < 0) {
+            die("Failed to send broadcast message");
+        }
+    }
+
+}
+
+
+
+// Handler for SIGINT signal
+void SIGINT_handler(int signum) {
+    if(signum == SIGINT) {
+        // std::string input;
+
+        // Prompt users before terminating
+        // std::cout << "Do you want to terminate? ";
+        // std::getline(std::cin, input);
+
+        // if(input[0] == 'y'){
+            // Shutdown socket before exiting
+            sendUDPMessage(CLOSING);
+            shutdown(udpSockFd, 0);
+            exit(0);
+        // }
+    }
+}
+
+
+
+void parseOptions(int argc, char** argv) {
     // Get default hostname
     hostName = getHostName();
 
@@ -105,41 +402,50 @@ int main(int argc, char** argv) {
             }
         }
     }
-
-    dprint("Username = %s\n", userName.c_str());
-    // TODO: Hostname = sp4.cs.ucdavis.edu
-    dprint("Hostname = %s\n", hostName.c_str());
-    dprint("UDP Port = %d\n", udpPort);
-    dprint("TCP Port = %d\n", tcpPort);
-    dprint("Mintimeout = %d\n", minTimeout);
-    dprint("Maxtimeout = %d\n", maxTimeout);
+}
 
 
+
+void initUDPMsg() {
     // Construct discovery message
-    int discoveryMsgLen = 10 + hostName.length() + 1 + userName.length() + 1;
-    uint8_t discoveryMsg[discoveryMsgLen], replyMsg[MAX_REPLY_MSG_LEN];
-    memset(discoveryMsg, 0, discoveryMsgLen);
+    udpMsgLen = 10 + hostName.length() + 1 + userName.length() + 1;
+    memset(udpMsg, 0, MAX_UDP_MSG_LEN);
 
-    memcpy((uint32_t*)discoveryMsg, "P2PI", 4);
-    *((uint16_t*)discoveryMsg + 2) = htons(1);
-    *((uint16_t*)discoveryMsg + 3) = htons(udpPort);
-    *((uint16_t*)discoveryMsg + 4) = htons(tcpPort);
-    memcpy(discoveryMsg + 10, hostName.c_str(), hostName.length());
-    memcpy(discoveryMsg + 10 + hostName.length() + 1, userName.c_str(), 
+    memcpy((uint32_t*)udpMsg, "P2PI", 4);
+    *((uint16_t*)udpMsg + 2) = htons(DISCOVERY);
+    *((uint16_t*)udpMsg + 3) = htons(udpPort);
+    *((uint16_t*)udpMsg + 4) = htons(tcpPort);
+    memcpy(udpMsg + 10, hostName.c_str(), hostName.length());
+    memcpy(udpMsg + 10 + hostName.length() + 1, userName.c_str(), 
         userName.length());
+}
 
-    // for(int i = 0; i < discoveryMsgLen; i++)
-    // {
-    //     dprint("%c", discoveryMsg[i]);
-    // }
-    // dprint("\n", 0);
 
-    int recvReply = 0;
 
-    // Send discovery message
+void addNewHost(uint8_t* incomingMsg) { 
+    struct Host newHost;
+    getHostNUserName(incomingMsg, newHost.hostName, newHost.userName);
+    getPorts(incomingMsg, newHost.udpPort, newHost.tcpPort);
+
+    hostMap[newHost.userName + "@" + newHost.hostName + ":" + std::to_string(newHost.tcpPort)] = newHost;
+}
+
+
+bool findHost(uint8_t* incomingMsg) {
+    struct Host newHost;
+    getHostNUserName(incomingMsg, newHost.hostName, newHost.userName);
+    getPorts(incomingMsg, newHost.udpPort, newHost.tcpPort);
+    
+    return hostMap.find(newHost.userName + "@" + newHost.hostName + ":" + std::to_string(newHost.tcpPort)) != hostMap.end();
+}
+
+
+
+void setupSocket() {
     udpSockFd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    tcpSockFd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-    if(udpSockFd < 0)
+    if(udpSockFd < 0 || tcpSockFd < 0)
         die("Failed to open socket");
 
     // Enable broadcast capability
@@ -156,186 +462,10 @@ int main(int argc, char** argv) {
         die("Failed to set socket option reuse address");
     }
 
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = htonl(INADDR_BROADCAST);
-    serverAddr.sin_port = htons(udpPort);
+    udpServerAddr.sin_family = AF_INET;
+    udpServerAddr.sin_addr.s_addr = htonl(INADDR_BROADCAST);
+    udpServerAddr.sin_port = htons(udpPort);
 
-    if(-1 == bind(udpSockFd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)))
+    if(-1 == bind(udpSockFd, (struct sockaddr*)&udpServerAddr, sizeof(udpServerAddr)))
         die("Failed to bind socket");
-    
-
-    pollFd.fd = udpSockFd;
-    pollFd.events = POLLIN;
-    int baseTimeout = minTimeout;
-    timeval start,end;
-    int timePassed;
-    int currTimeout = 0;
-
-
-    while(!recvReply && currTimeout <= maxTimeout * 1000) {
-        
-        if(currTimeout <= 0) {
-            if(sendto(udpSockFd, discoveryMsg, discoveryMsgLen, 0, 
-                (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
-                die("Failed to send discovery message");
-
-            }
-            currTimeout=baseTimeout;
-            // dprint("YO\n", 0);
-        }
-
-
-        // Wait for reply message
-    
-        gettimeofday(&start, NULL);
-        int rc = poll(&pollFd, 1, currTimeout);
-        gettimeofday(&end, NULL);
-        timePassed = ((end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec) / 1000;
-        currTimeout -= timePassed;
-
-        // Timeout event
-        if(0 == rc) {
-            dprint("TIMEOUT\n", 0);
-
-            // Double current timeout
-            baseTimeout *= 2;
-            dprint("%d\n", currTimeout);
-
-        }
-        else if(rc > 0 && POLLIN == pollFd.revents) {
-            std::string newHostName, newUserName;
-
-            dprint("RECV\n", 0);
-            // Reply message
-            clientAddrLen = sizeof(clientAddr);
-            int recvLen = recvfrom(udpSockFd, replyMsg, MAX_REPLY_MSG_LEN, 0,
-                (struct sockaddr*)&clientAddr, &clientAddrLen);
-
-            if(recvLen > 0) {
-                getHostNUserName(replyMsg, newHostName, newUserName);
-                dprint("%s %s\n", newHostName.c_str(), newUserName.c_str());
-            }
-
-            if(newHostName == hostName)
-                dprint("Self discovery\n", 0);
-            else
-                recvReply = 1;
-        }
-        else
-            dprint("ERROR\n", 0);
-    }
-
-
-    uint8_t closingMsg[sizeof(discoveryMsg)];
-    memcpy(closingMsg, discoveryMsg, sizeof(discoveryMsg));
-
-    *((uint16_t*)closingMsg + 2) = htons(3);  
-
-    if(sendto(udpSockFd, closingMsg, discoveryMsgLen, 0, 
-        (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
-        die("Failed to send discovery message");
-    }
-
-    dprint("Bye...\n", 0);
-
-    struct termios SavedTermAttributes;
-    char RXChar;
-    
-   /*  SetNonCanonicalMode(STDIN_FILENO, &SavedTermAttributes);
-    
-    while(1){
-        read(STDIN_FILENO, &RXChar, 1);
-        if(0x04 == RXChar){ // C-d
-            break;
-        }
-        else{
-            if(isprint(RXChar)){
-                printf("RX: '%c' 0x%02X\n",RXChar, RXChar);   
-            }
-            else{
-                printf("RX: ' ' 0x%02X\n",RXChar);
-            }
-        }
-    }
-    
-    ResetCanonicalMode(STDIN_FILENO, &SavedTermAttributes); */
-
-
-    
-    return 0;
-}
-
-
-
-void optionError(char** argv){
-    fprintf(stderr, "%s: option requires an argument -- '%s'\n", argv[0], optErr.c_str());
-    exit(1);
-}
-
-
-
-std::string getHostName()
-{
-    char buffer[256];
-    
-    if(-1 == gethostname(buffer, 255)){
-        fprintf(stderr, "Unable to resolve host name.");
-        exit(-1);
-    }
-
-    struct hostent* localHostEntry = gethostbyname(buffer);
-    strcpy(buffer, localHostEntry->h_name);
-    return std::string(buffer);
-}
-
-
-
-// Internal syscall errors
-void die(const char *message) {
-    perror(message);
-    exit(1);
-}
-
-
-
-int getType(uint8_t* message) {
-    return ntohs(*((uint16_t*)message + 2));
-}
-
-
-
-void getHostNUserName(uint8_t* message, std::string& hostName, std::string& userName) {
-    hostName = (char*)(message + 10);
-    userName = (char*)(message + 10 + hostName.length() + 1);
-}
-
-
-
-void dump(std::string msg)
-{
-    for(int i = 0; i < msg.length(); i++)
-    {
-        dprint("%d %c \n", msg[i], msg[i]);
-    }
-    dprint("\n", 0);
-}
-
-
-
-void checkIsNum(char* str) {
-    for(int i = 0; i < strlen(str); i++) {
-        if(!isdigit(str[i])) {
-            fprintf(stderr, "Input %s is not a number!\n", str);
-            exit(1);
-        }
-    }
-}
-
-
-
-void checkPortRange(int portNum) {
-    if(1 > portNum || 65535 < portNum) {
-        fprintf(stderr, "Invalid port \"%d\", must be in range [1 65,535]\n", portNum);
-        exit(1);
-    }
 }
--- p2pim.h
+++ p2pim.h
@@ -20,6 +20,7 @@
 #include <poll.h>
 #include <sys/time.h>
 #include <cctype>
+#include <signal.h>
 
 
 
@@ -32,12 +33,37 @@
 #endif
 
 
+
+#define DISCOVERY 1
+#define REPLY 2
+#define CLOSING 3
+#define ESTABLISH_COMM 4
+#define ACCEPT_COMM 5
+#define USER_UNAVALIBLE 6
+#define REQUEST_USER_LIST 7
+#define REPLY_USER_LIST 8
+#define DATA 9
+#define DISCONTINUE_COMM 10
+
+
+
 void ResetCanonicalMode(int fd, struct termios *savedattributes);
 void SetNonCanonicalMode(int fd, struct termios *savedattributes);
 void optionError(char** argv);
 void die(const char* message);
 int getType(uint8_t* message);
 void getHostNUserName(uint8_t* message, std::string& hostName, std::string& userName);
+void getPorts(uint8_t* message, int& udpPort, int& tcpPort);
+
+
+
 std::string getHostName();
 void checkIsNum(char* str);
 void checkPortRange(int portNum);
+void SIGINT_handler(int signum);
+void parseOptions(int argc, char** argv);
+void initUDPMsg();
+void addNewHost(uint8_t* replyMsg);
+void setupSocket();
+void sendUDPMessage(int type);
+bool findHost(uint8_t* replyMsg);
